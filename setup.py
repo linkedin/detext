a81e46598d97d2ea748a3183c22047b2e547d611
Xiaowei Liu <xwli@linkedin.com>
2020-04-21 12:37:56 -0700

    [detext] Prepare package for PyPi (#4)
    * [detext] Prepare package for PyPi
    
    * Releasing instructions for dev team
    
    Instructions for dev team on new releasing.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 setuptools.setup(
     name='detext',
-    version='1.0.1',
+    version='1.0.7',
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     include_package_data=True,
