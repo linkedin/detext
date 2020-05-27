ca61b24f76b67007475928e014a52101be530a8b
cyzhao2013 <joesphzhao2013@gmail.com>
2020-05-27 13:36:26 -0400

    override the steps_per_eval when num_eval_rounds provided (#18)
    * overwrited the step_per_eval when total_eval_steps provided
    
    * change the name of param to num_eval_round
    
    * update the parameter discription
    
    Co-authored-by: Changyong Zhao <czhao@czhao-mn1.linkedin.biz>

--- src/detext/run_detext.py
+++ src/detext/run_detext.py
@@ -76,7 +76,8 @@ def add_arguments(parser):
     parser.add_argument("--max_gradient_norm", type=float, default=1.0, help="Clip gradients to this norm.")
     parser.add_argument("--learning_rate", type=float, default=1.0, help="Learning rate. Adam: 0.001 | 0.0001")
     parser.add_argument("--num_train_steps", type=int, default=1, help="Num steps to train.")
-    parser.add_argument("--num_epochs", type=int, default=None, help="Num of epochs to train, will overwrite train_steps if set")
+    parser.add_argument("--num_epochs", type=int, default=None,
+                        help="Num of epochs to train, will overwrite train_steps if set")
     parser.add_argument("--num_warmup_steps", type=int, default=0, help="Num steps for warmup.")
     parser.add_argument("--train_batch_size", type=int, default=32, help="Training data batch size.")
     parser.add_argument("--test_batch_size", type=int, default=32, help="Test data batch size.")
@@ -114,6 +115,9 @@ def add_arguments(parser):
     # Misc
     parser.add_argument("--random_seed", type=int, default=1234, help="Random seed (>0, set a specific seed).")
     parser.add_argument("--steps_per_stats", type=int, default=100, help="training steps to print statistics.")
+    parser.add_argument("--num_eval_rounds", type=int, default=None, help="number of evaluation round,this param will "
+                                                                          "override steps_per_eval as max(1,"
+                                                                          "num_train_steps / num_eval_rounds)")
     parser.add_argument("--steps_per_eval", type=int, default=1000, help="training steps to evaluate datasets.")
     parser.add_argument("--keep_checkpoint_max", type=int, default=5,
                         help="The maximum number of recent checkpoint files to keep. If 0, all checkpoint "
@@ -144,7 +148,8 @@ def add_arguments(parser):
                         help="softmax_loss")
     parser.add_argument('--tfr_lambda_weights', type=str, default=None)
 
-    parser.add_argument('--use_horovod', type=str2bool, default=False, help="whether to use horovod for sync distributed training")
+    parser.add_argument('--use_horovod', type=str2bool, default=False,
+                        help="whether to use horovod for sync distributed training")
 
     # multitask training related
     parser.add_argument('--task_ids', type=str, default=None,
@@ -199,6 +204,7 @@ def create_hparams(flags):
         random_seed=flags.random_seed,
         steps_per_stats=flags.steps_per_stats,
         steps_per_eval=flags.steps_per_eval,
+        num_eval_rounds=flags.num_eval_rounds,
         keep_checkpoint_max=flags.keep_checkpoint_max,
         max_len=flags.max_len,
         min_len=flags.min_len,
@@ -284,6 +290,10 @@ def main(argv):
             hparams.num_epochs,
             hparams.train_batch_size,
             hparams.metadata_path is None)
+
+    # if num_eval_rounds is set, override steps_per_eval
+    if hparams.num_eval_rounds is not None:
+        hparams.steps_per_eval = max(1, int(hparams.num_train_steps / hparams.num_eval_rounds))
 
     # Create directory and launch tensorboard
     if task_type == executor_utils.CHIEF or task_type == executor_utils.LOCAL_MODE:
