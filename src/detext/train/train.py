797e7c911b6f1a95bd4f6b38544d092969fe8f49
Xiaowei Liu <xwli@linkedin.com>
2020-06-13 11:50:56 -0700

    Changing throttle_secs to be 0 to fix small data eval issue. (#27)
    * change throttle_sec to 0 to fix test job problems
    
    * change throttle_sec to 0 to fix test job problems

--- src/detext/train/train.py
+++ src/detext/train/train.py
@@ -73,9 +73,10 @@ def train(hparams, input_fn):
         input_fn=input_fn_common(hparams.dev_file),
         exporters=exporter_list,
         steps=None,
-        # Set throttle_secs to 10 min to avoid warning to spam logs
-        # Set throttle to 0 for horovod: https://github.com/horovod/horovod/issues/182#issuecomment-533897757
-        throttle_secs=0 if hvd else 600,
+        # Set throttle to 0 to start evaluation right away.
+        # (Note: throttle_secs has to be 0 for horovod:
+        # https://github.com/horovod/horovod/issues/182#issuecomment-533897757)
+        throttle_secs=0,
         start_delay_secs=10)
 
     # Training and evaluation with dev set
