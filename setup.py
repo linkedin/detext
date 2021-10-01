2866498a609b62610730b47fcce3d8f0ad28fe3e
Anurag Kumar <mailanu98@gmail.com>
2021-10-02 00:14:59 +0530

    Update setup.py
    Updated classifiers

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=["Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9",
                  "Topic :: Scientific/Engineering :: Artificial Intelligence",
                  "Topic :: Software Development :: Libraries",
                  "Intended Audience :: Science/Research",
